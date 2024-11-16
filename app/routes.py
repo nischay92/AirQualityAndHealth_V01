bf709cecd36dbd662bdfcdf9eeb1d34396ea4f04
nischay92 <nischayn92@gmail.com>
2024-11-16 03:44:19 -0500

    Initial Commit - Base code commited for the dashboard development

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+# Create a Blueprint for routes
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('index.html', title='Air Quality Dashboard')
